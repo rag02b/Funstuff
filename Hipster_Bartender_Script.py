08d1dbc42b21831e890a1ea007cfe7eaa9a349c7
rag02b <rag02b@gmail.com>
2014-09-22 23:00:56 -0800

    Hipster Bartender Script
    Are people over the age of 35 welcome in hipster bars?

---
+++ Hipster_Bartender_Script.py
@@ -0,0 +1,14 @@
+# Hipster Bartender Script
+
+print "How old are you?"
+
+age = input()
+
+if age <= 20:
+	print "Get outta here."
+elif age >= 21 and age <= 35:
+	print "Sup"
+elif age > 35:
+	print "Can I help you sir?"
+else:
+	"Null"
