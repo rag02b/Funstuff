6317693bb5aed7d8a16a08d76c68d75748351a86
rag02b <rag02b@gmail.com>
2014-09-25 16:26:05 -0800

    Add Rock, Paper, Scissors

---
+++ simplerock_paper_scissors.py
@@ -0,0 +1,36 @@
+import random
+import time
+#simple version of rock, paper, scissors
+choices = ["rock", "paper", "scissors"] #choices in the game
+userchoice = raw_input("Do you choose paper, scissors, or rock?  ")
+compchoice = (random.choice(choices)) #computer chooses random choice
+
+time.sleep(1)
+
+print("The computer chooses ")
+print(compchoice)
+
+time.sleep(1)
+
+def game1(choice1, choice2):
+	if (choice1 == choice2):
+		print("The game is a tie!");
+	elif (choice1 == "rock"):
+		if (choice2 == "scissors"):
+			print("rock wins")
+		else:
+			print("paper wins");
+	elif (choice1 == "paper"):
+		if (choice2 == "rock"):
+			print("paper wins")
+		else:
+			print("scissors wins");
+	elif (choice1 == "scissors"):
+		if (choice2 == "paper"):
+			print("scissors wins")
+		else:
+			print("rock wins")
+			
+			
+game1(userchoice, compchoice)
+
