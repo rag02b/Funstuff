fb2eea1afbd5fd04699848a87f390ffd1b9e6c8a
rag02b <rag02b@gmail.com>
2014-10-16 14:27:57 -0800

    Edits

--- simplerock_paper_scissors.py
+++ simplerock_paper_scissors.py
@@ -2,7 +2,7 @@ import random
 import time
 #simple version of rock, paper, scissors
 choices = ["rock", "paper", "scissors"] #choices in the game
-userchoice = raw_input("Do you choose paper, scissors, or rock?  ")
+userchoice = raw_input("Do you choose paper, scissors, or rock?  ") #user chooses
 compchoice = (random.choice(choices)) #computer chooses random choice
 
 time.sleep(1)
@@ -29,7 +29,9 @@ def game1(choice1, choice2):
 		if (choice2 == "paper"):
 			print("scissors wins")
 		else:
-			print("rock wins")
+			print("rock wins");
+	else:
+		print("What?")
 			
 			
 game1(userchoice, compchoice)
